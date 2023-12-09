7eadc1d4eda2e4c2e457362472c15d9252479e5d
dipnapt <151124883+dipnapt@users.noreply.github.com>
2023-12-09 15:30:42 +0700

    Update sumfrnews.py

--- sumfrnews.py
+++ sumfrnews.py
@@ -2,9 +2,9 @@ import openai
 import streamlit as st
 import json
 import pandas as pd
+import numpy as np
 
-st.sidebar.text_input("OpenAI API key", type="password")
-user_api_key = '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'
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """You need to extract some words from the given passage. 
             You will receive a passage from user input
@@ -21,16 +21,6 @@ prompt = """You need to extract some words from the given passage.
 st.title('Helpful French Vocab')
 st.markdown('give AI the passage you want to learn the vocabulaire. wait a minute for the helpful result! fyi : the given vocabulaire is for intermediate level')
         
-
-#user_input = """L'année 1866 fut marquée par un événement bizarre, 
-#                un phénomène inexpliqué et inexplicable que personne 
-#                n'a sans doute oublié. Sans parler des rumeurs qui agitaient 
-#                les populations des ports et surexcitaient l'esprit public à 
-#                l'intérieur des continents, les gens de mer furent particulièrement émus. 
-#                Les négociants, armateurs, capitaines de navires, skippers et masters de 
-#                l'Europe et de l'Amérique, officiers des marines militaires de tous pays, et, 
-#                après eux, les gouvernements des divers États des deux continents, 
-#                se préoccupèrent de ce fait au plus haut point."""
 user_input =st.text_input("Enter your French passage: ","French Passage here")
 if st.button('Submit'):
     messages_so_far = [
@@ -49,5 +39,6 @@ if st.button('Submit'):
     print(res_dict)
     
     vocab_tableau = pd.DataFrame.from_dict(res_dict)
+    vocab_tableau = np.arange(1, len(vocab_tableau) + 1)
     print(vocab_tableau)
     st.table(vocab_tableau)
